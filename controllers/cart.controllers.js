54d496a1330251a3850389a882311602b2c9a3e1
Arfin Dwi Octavianto <arfindwioctavianto@gmail.com>
2024-04-24 19:52:38 +0700

    feat: Add GET, POST, PUT, and Delete endpoints & swagger open api for cart

---
+++ controllers/cart.controllers.js
@@ -0,0 +1,144 @@
+const prisma = require("../libs/prismaClient");
+const catchAsync = require("../utils/catchAsync");
+const { CustomError } = require("../utils/errorHandler");
+const { formattedDate } = require("../utils/formattedDate");
+
+module.exports = {
+  getAllCartsByAuth: catchAsync(async (req, res, next) => {
+    try {
+      const carts = await prisma.cart.findMany({
+        where: { userId: Number(req.user.id), paymentId: null },
+      });
+
+      res.status(200).json({
+        status: true,
+        message: "show all carts successful",
+        data: { carts },
+      });
+    } catch (err) {
+      next(err);
+    }
+  }),
+
+  createCartByProductId: catchAsync(async (req, res, next) => {
+    try {
+      const { productId } = req.params;
+      const { note, sizeId, colorId } = req.body;
+
+      const product = await prisma.product.findUnique({
+        where: { id: Number(productId) },
+        include: {
+          size: true,
+          color: true,
+        },
+      });
+
+      if (!product || !product.size.find((s) => s.id === Number(sizeId)) || !product.color.find((c) => c.id === Number(colorId))) {
+        throw new CustomError(404, "Product, size, or color not found.");
+      }
+
+      let newCart = await prisma.cart.findFirst({
+        where: {
+          productId: Number(productId),
+          sizeId: Number(sizeId),
+          colorId: Number(colorId),
+        },
+      });
+
+      if (newCart) {
+        newCart = await prisma.cart.update({
+          where: {
+            id: Number(newCart.id),
+          },
+          data: {
+            note,
+            quantity: { increment: 1 },
+            updatedAt: formattedDate(new Date()),
+          },
+        });
+      } else {
+        newCart = await prisma.cart.create({
+          data: {
+            quantity: 1,
+            note,
+            sizeId: Number(sizeId),
+            colorId: Number(colorId),
+            productId: Number(productId),
+            userId: Number(req.user.id),
+            createdAt: formattedDate(new Date()),
+            updatedAt: formattedDate(new Date()),
+          },
+        });
+      }
+
+      res.status(201).json({
+        status: true,
+        message: "Create cart successful",
+        data: { newCart },
+      });
+    } catch (err) {
+      next(err);
+    }
+  }),
+
+  editCartById: catchAsync(async (req, res, next) => {
+    try {
+      const { cartId } = req.params;
+      const { quantity, note } = req.body;
+
+      if (!quantity) throw new CustomError(400, "Please provide quantity");
+
+      const cart = await prisma.cart.findUnique({
+        where: { id: Number(cartId) },
+      });
+
+      if (!cart) throw new CustomError(404, "cart Not Found");
+
+      let editedCart = await prisma.cart.update({
+        where: {
+          id: Number(cart.id),
+        },
+        data: {
+          quantity,
+          note,
+          productId: Number(cart.productId),
+          updatedAt: formattedDate(new Date()),
+        },
+      });
+
+      res.status(200).json({
+        status: true,
+        message: "update cart successful",
+        data: { editedCart },
+      });
+    } catch (err) {
+      next(err);
+    }
+  }),
+
+  deleteCartById: catchAsync(async (req, res, next) => {
+    try {
+      const { cartId } = req.params;
+
+      const cart = await prisma.cart.findUnique({
+        where: { id: Number(cartId) },
+      });
+
+      if (!cart) throw new CustomError(404, "cart Not Found");
+
+      const deletedCart = await prisma.cart.delete({
+        where: {
+          id: Number(cart.id),
+        },
+      });
+
+      res.status(200).json({
+        status: true,
+        message: "delete cart successful",
+        data: { deletedCart },
+      });
+    } catch (err) {
+      next(err);
+    }
+  }),
+};
